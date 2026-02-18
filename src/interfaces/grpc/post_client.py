8d7201d028f76b5c0b252a8d5d9ffd74868f1f15
colosss <kocnevs.jk@yandex.ru>
2026-02-18 18:39:21 +0300

    Add grpc

---
+++ src/interfaces/grpc/post_client.py
@@ -0,0 +1,16 @@
+import asyncio
+import logging
+
+import grpc
+import pb.post_pb2 as pb2
+import pb.post_pb2_grpc as pb2_grpc
+
+async def run()->None:
+    async with grpc.aio.insecure_channel("localhost:50051") as ch:
+        async_stub=pb2_grpc.GreeterAsyncStub(ch)
+        response=await async_stub.GetUserName(pb2.PostRequest(post_id=1))
+    print("Greeter client received: "+response.message)
+
+def main():
+    logging.basicConfig()
+    asyncio.run(run())
